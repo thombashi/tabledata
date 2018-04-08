5ad7fa3a4a206aaf88038f7e6b3a5a955d20c8d7
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-08 20:54:29 +0900

    Modify calculation

--- tabledata/_common.py
+++ tabledata/_common.py
@@ -9,6 +9,7 @@ def convert_idx_to_alphabet(column_idx):
     if column_idx < 26:
         return chr(65 + column_idx)
 
+    div, mod = divmod(column_idx, 26)
+
     return (
-        convert_idx_to_alphabet(int(column_idx / 26 - 1)) +
-        convert_idx_to_alphabet(column_idx % 26))
+        convert_idx_to_alphabet(div - 1) + convert_idx_to_alphabet(mod))
