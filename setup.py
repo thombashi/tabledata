56a80a7dfc5adc9efcfd52580176cebd0e43c82b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-08-19 12:49:48 +0900

    Change URLs from http to https

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
-        "Documentation": "http://{:s}.rtfd.io/".format(MODULE_NAME),
+        "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
         "Tracker": "{:s}/issues".format(REPOSITORY_URL),
     },
 
