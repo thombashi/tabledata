18df7b2ae7bd4e9f2809b8cea86be616e3df5bd4
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-05-02 12:21:47 +0900

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,14 @@
 import os
 import sys
 
-import sphinx_rtd_theme
 from tabledata import __author__, __copyright__, __name__, __version__
+
+
+try:
+    import sphinx_rtd_theme
+    RTD_THEME_INSTALLED = True
+except ImportError:
+    RTD_THEME_INSTALLED = False
 
 
 sys.path.insert(0, os.path.abspath('../tabledata'))
@@ -78,9 +84,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if RTD_THEME_INSTALLED:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
