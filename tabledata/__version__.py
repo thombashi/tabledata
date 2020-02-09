6486f8bcabed4ec01abd30bc34edba7dc9796ae0
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-09 13:01:49 +0900

    Bump version

--- tabledata/__version__.py
+++ tabledata/__version__.py
@@ -3,6 +3,6 @@
 __author__ = "Tsuyoshi Hombashi"
 __copyright__ = "Copyright 2017, {}".format(__author__)
 __license__ = "MIT License"
-__version__ = "0.9.2"
+__version__ = "0.10.0"
 __maintainer__ = __author__
 __email__ = "tsuyoshi.hombashi@gmail.com"
