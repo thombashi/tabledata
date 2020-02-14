afed47ace08dfc7748fa200b6497a1869ca0b062
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-14 22:58:46 +0900

    Fix NullLogger

--- test/test_logger.py
+++ test/test_logger.py
@@ -5,9 +5,17 @@ from __future__ import print_function, unicode_literals
 import pytest
 
 from tabledata import set_logger
+from tabledata._logger._null_logger import NullLogger
 
 
 class Test_set_logger(object):
     @pytest.mark.parametrize(["value"], [[True], [False]])
     def test_smoke(self, value):
+        set_logger(value)
+
+
+class Test_NullLogger:
+    @pytest.mark.parametrize(["value"], [[True], [False]])
+    def test_smoke(self, value, monkeypatch):
+        monkeypatch.setattr("tabledata._logger._logger.logger", NullLogger())
         set_logger(value)
